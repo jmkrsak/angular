5dc1210a7710aa65555cda16970cb82aebe911f2
John-Michael Krsak <jmkrsak@gmail.com>
2023-03-13 22:44:30 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 //adding backend
+//excited to stop working on algorithms and start working on the backend
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
